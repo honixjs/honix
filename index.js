9b5f55b00bddf96eda8f7b31599722d44ae80ff8
ahsouza <annibalhsouza@gmail.com>
2022-05-19 20:07:11 -0300

    feat: add main

---
+++ index.js
@@ -0,0 +1,15 @@
+/*!
+-----------------------------------------------
+  Framework JS server-side
+      
+      GitHub: https://github.com/eloajs
+      E-Mail: support@eloajs.com.br
+
+      Author: Aníbal Henrique Souza
+          
+          Copyright © Eloa.js 2022 
+------------------------------------------------
+ */
+'use strict';
+
+module.exports = require('./lib/eloa');
