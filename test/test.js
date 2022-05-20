c32f50b76ce141bd05701073147d13bf87f5424b
ahsouza <annibalhsouza@gmail.com>
2022-05-20 00:25:19 -0300

    test: add new examples

--- test/test.js
+++ test/test.js
@@ -1,7 +1,13 @@
-const app = require("../lib/eloa.js");
+const app = require("eloajs");
 
-
+// example of how to create http server
 var test = app.onServer({
-   write: "Hello, world",
-   port: 8000
+  write: "Hello, world",
+  port: 8000
 });
+
+// example of how to generate log messages with colors
+app.log("hello, I'm using eloa.js", { color: "green"})
+
+// example of how to allow to write log messages
+app.log("hello, I'm using eloa.js", { record: true})
